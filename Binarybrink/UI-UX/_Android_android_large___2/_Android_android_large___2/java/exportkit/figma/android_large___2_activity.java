a03037d1e33c687c1caa563e5012ab6641b6a351
Freak3123 <118867504+Freak3123@users.noreply.github.com>
2024-02-20 21:46:58 +0530

    Add files via upload
    page 2

---
+++ Binarybrink/UI-UX/_Android_android_large___2/_Android_android_large___2/java/exportkit/figma/android_large___2_activity.java
@@ -0,0 +1,118 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_large___2
+	 *	@date 		Tuesday 20th of February 2024 03:23:30 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class android_large___2_activity extends Activity {
+
+	
+	private View _bg__android_large___2;
+	private View rectangle_1;
+	private View _bg__group_13_ek1;
+	private TextView _2024;
+	private View _bg______icon__arrow_drop_down__ek1;
+	private ImageView vector;
+	private ImageView vector_ek1;
+	private View _bg__group_12_ek1;
+	private TextView february;
+	private View _bg______icon__arrow_drop_down__ek3;
+	private ImageView vector_ek2;
+	private ImageView vector_ek3;
+	private View _bg__group_11_ek1;
+	private TextView today;
+	private View _bg______icon__arrow_drop_down__ek5;
+	private ImageView vector_ek4;
+	private ImageView vector_ek5;
+	private View _bg_______icon__navigate_next__ek1;
+	private ImageView vector_ek6;
+	private ImageView vector_ek7;
+	private TextView battery_used_history;
+	private View _bg__group_19_ek1;
+	private View _bg__group_16_ek1;
+	private TextView _33_;
+	private TextView battery;
+	private View _bg__group_17_ek1;
+	private TextView _32_001;
+	private TextView tot__kwh;
+	private View _bg__group_18_ek1;
+	private TextView rs_360;
+	private TextView expend;
+	private View rectangle_4;
+	private TextView gathering_usage_information_for_analysis_and_optimization_of_electric_vehicle_performance_;
+	private View _bg__group_20_ek1;
+	private View rectangle_5;
+	private TextView download_data;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_large___2);
+
+		
+		_bg__android_large___2 = (View) findViewById(R.id._bg__android_large___2);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		_bg__group_13_ek1 = (View) findViewById(R.id._bg__group_13_ek1);
+		_2024 = (TextView) findViewById(R.id._2024);
+		_bg______icon__arrow_drop_down__ek1 = (View) findViewById(R.id._bg______icon__arrow_drop_down__ek1);
+		vector = (ImageView) findViewById(R.id.vector);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		_bg__group_12_ek1 = (View) findViewById(R.id._bg__group_12_ek1);
+		february = (TextView) findViewById(R.id.february);
+		_bg______icon__arrow_drop_down__ek3 = (View) findViewById(R.id._bg______icon__arrow_drop_down__ek3);
+		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+		vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
+		_bg__group_11_ek1 = (View) findViewById(R.id._bg__group_11_ek1);
+		today = (TextView) findViewById(R.id.today);
+		_bg______icon__arrow_drop_down__ek5 = (View) findViewById(R.id._bg______icon__arrow_drop_down__ek5);
+		vector_ek4 = (ImageView) findViewById(R.id.vector_ek4);
+		vector_ek5 = (ImageView) findViewById(R.id.vector_ek5);
+		_bg_______icon__navigate_next__ek1 = (View) findViewById(R.id._bg_______icon__navigate_next__ek1);
+		vector_ek6 = (ImageView) findViewById(R.id.vector_ek6);
+		vector_ek7 = (ImageView) findViewById(R.id.vector_ek7);
+		battery_used_history = (TextView) findViewById(R.id.battery_used_history);
+		_bg__group_19_ek1 = (View) findViewById(R.id._bg__group_19_ek1);
+		_bg__group_16_ek1 = (View) findViewById(R.id._bg__group_16_ek1);
+		_33_ = (TextView) findViewById(R.id._33_);
+		battery = (TextView) findViewById(R.id.battery);
+		_bg__group_17_ek1 = (View) findViewById(R.id._bg__group_17_ek1);
+		_32_001 = (TextView) findViewById(R.id._32_001);
+		tot__kwh = (TextView) findViewById(R.id.tot__kwh);
+		_bg__group_18_ek1 = (View) findViewById(R.id._bg__group_18_ek1);
+		rs_360 = (TextView) findViewById(R.id.rs_360);
+		expend = (TextView) findViewById(R.id.expend);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		gathering_usage_information_for_analysis_and_optimization_of_electric_vehicle_performance_ = (TextView) findViewById(R.id.gathering_usage_information_for_analysis_and_optimization_of_electric_vehicle_performance_);
+		_bg__group_20_ek1 = (View) findViewById(R.id._bg__group_20_ek1);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+		download_data = (TextView) findViewById(R.id.download_data);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
