bdd6838c8ad8c75d29ec7c0c01efbb9d275aa497
Aniket Jena <ajempire09@gmail.com>
2024-02-21 01:45:15 +0530

    Configured login, register mutations and me query using swr

--- Deadsec/co-lab-client/components/Header.tsx
+++ Deadsec/co-lab-client/components/Header.tsx
@@ -1,7 +1,41 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import useSWR, { mutate } from "swr";
+import useSWRMutation from "swr/mutation";
 
 function Header() {
+	const me = async () => {
+		const res = await fetch("http://localhost:4000/me", {
+			credentials: "include",
+		});
+		return await res.json();
+	};
+
+	const { data } = useSWR("me", me);
+
+	const logout = async (_: unknown) => {
+		const res = await fetch("http://localhost:4000/logout", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			credentials: "include",
+		});
+
+		return await res.json();
+	};
+
+	// mutation trigger
+	const { trigger } = useSWRMutation("logout", logout, {
+		onSuccess: () => {
+			mutate("me");
+		},
+	});
+
+	const handleLogout = async () => {
+		await trigger();
+	};
+
 	return (
 		<header className="flex items-center justify-between fixed w-full p-4">
 			<Link href={"/"} className="text-2xl font-bold">
@@ -9,12 +43,23 @@ function Header() {
 			</Link>
 
 			<nav className="navbar-end w-fit space-x-2">
-				<Link href={"/login"} className="btn btn-primary">
-					Login
-				</Link>
-				<Link href={"/register"} className="btn btn-secondary btn-outline">
-					Register
-				</Link>
+				{data?.user?.id ? (
+					<>
+						<strong>{data?.user?.name}</strong>
+						<button className="btn btn-error" onClick={handleLogout}>
+							Logout
+						</button>
+					</>
+				) : (
+					<>
+						<Link href={"/login"} className="btn btn-primary">
+							Login
+						</Link>
+						<Link href={"/register"} className="btn btn-secondary btn-outline">
+							Register
+						</Link>
+					</>
+				)}
 			</nav>
 		</header>
 	);
