5ccf8d5da7e191b8c0ce76ae31737e52b5026d7e
Yajy <goswamiyajy@gmail.com>
2024-02-21 02:57:21 +0530

    registration java file

--- Team Patrologix/app/build.gradle.kts
+++ Team Patrologix/app/build.gradle.kts
@@ -32,10 +32,18 @@ android {
 }
 
 dependencies {
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.squareup.picasso:picasso:2.71828")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-analytics:21.5.0")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- Team Patrologix/app/src/main/java/com/example/sentinex/login.java
+++ Team Patrologix/app/src/main/java/com/example/sentinex/login.java
@@ -1,7 +1,48 @@
 package com.example.sentinex;
 
-public class login {
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-    //1st commit
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.Firebase;
+
+public class login extends AppCompatActivity {
+
+
 
 }
+
+//
+//package com.example.sentenix;
+//
+//public class Report {
+//    private String description;
+//    private String location;
+//
+//    public Report() {
+//        // Default constructor required for Firebase Realtime Database
+//    }
+//
+//    public Report(String description, String location) {
+//        this.description = description;
+//        this.location = location;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public String getLocation() {
+//        return location;
+//    }
+//}
+
