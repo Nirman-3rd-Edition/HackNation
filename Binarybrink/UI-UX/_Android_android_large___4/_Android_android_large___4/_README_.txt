a6b0ef964c71c9103d82e240d08f68aa11b6234d
Freak3123 <118867504+Freak3123@users.noreply.github.com>
2024-02-20 22:45:33 +0530

    Add files via upload

---
+++ Binarybrink/UI-UX/_Android_android_large___4/_Android_android_large___4/_README_.txt
@@ -0,0 +1,22 @@
+Export Kit - Lightning Storm CC
+Android Studio Projects Update (July 25, 2019):
+
+  1. Copy all files and folders in your "Page 1" 
+     export directory (this folder).
+
+  2. Locate your Android Studio Projects folder.  
+
+     Default Locations
+     -----------------
+     WIN: %userprofile%\StudioProjects
+     MAC: ~\StudioProjects
+
+  3. Create a new Android Studio Project with the name 
+     "Page 1" and ensure you are using 
+     "Add no Activity" during the Activity Settings.
+
+  4. Paste and *replace all* files and folders located in 
+     your project folder { Page 1/app/src/main } with 
+     the contents copied in Step 1.
+
+  5. Done!
---
+++ Binarybrink/UI-UX/_Android_android_large___4/java/exportkit/figma/android_large___4_activity.java
@@ -0,0 +1,174 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_large___4
+	 *	@date 		Tuesday 20th of February 2024 04:55:26 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class android_large___4_activity extends Activity {
+
+	
+	private View _bg__android_large___4;
+	private View rectangle_1;
+	private View _bg__group_13_ek1;
+	private TextView _2024;
+	private View _bg______icon__arrow_drop_down__ek1;
+	private ImageView vector;
+	private ImageView vector_ek1;
+	private View _bg__group_34_ek1;
+	private View _bg__group_33_ek1;
+	private View _bg__group_32_ek1;
+	private TextView feb;
+	private View _bg______icon__arrow_drop_down__ek3;
+	private ImageView vector_ek2;
+	private ImageView vector_ek3;
+	private View _bg_______icon__navigate_next__ek1;
+	private ImageView vector_ek4;
+	private ImageView vector_ek5;
+	private TextView gathering_usage_information_for_analysis_and_optimization_of_electric_vehicle_performance_;
+	private View _bg__group_20_ek1;
+	private View rectangle_5;
+	private TextView download_data;
+	private View _bg__group_22_ek1;
+	private TextView ola_s1;
+	private View _bg______icon__arrow_drop_down__ek5;
+	private ImageView vector_ek6;
+	private ImageView vector_ek7;
+	private TextView recent_orders;
+	private View _bg__frame_5_ek1;
+	private View _bg__group_45_ek1;
+	private View _bg__group_35_ek1;
+	private View rectangle_5_ek1;
+	private TextView order____87654321;
+	private TextView february_10__2024___2_00_pm;
+	private TextView suji_ka_halwa_charging_station_iter_square__bhubneshwar__odisha;
+	private View _bg__group_44_ek1;
+	private TextView success;
+	private TextView rs_700;
+	private View _bg__group_46_ek1;
+	private View _bg__group_35_ek3;
+	private View rectangle_5_ek2;
+	private TextView order____87654321_ek1;
+	private TextView february_10__2024___2_00_pm_ek1;
+	private TextView suraj_prem_charging_station_jio_nagar__bhubneshwar__odisha;
+	private View _bg__group_44_ek3;
+	private TextView success_ek1;
+	private TextView rs_126;
+	private View _bg__group_40_ek1;
+	private View rectangle_5_ek3;
+	private View _bg__group_35_ek5;
+	private TextView order____12348765;
+	private TextView february_18__2024___1_30_pm;
+	private TextView indhar_ka_ev_charging_station_saheed_nagar__bhubneshwar__odisha;
+	private View _bg__group_36_ek1;
+	private TextView rs_537;
+	private TextView terminated;
+	private View _bg__group_43_ek1;
+	private View rectangle_5_ek4;
+	private View _bg__group_41_ek1;
+	private TextView order____12345678;
+	private TextView february_21__2024___5_30_pm;
+	private TextView gajar_ka_halwa_charging_station_khao_gali__bhubneshwar__odisha;
+	private View _bg__group_42_ek1;
+	private TextView charging___;
+	private TextView on_hold;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_large___4);
+
+		
+		_bg__android_large___4 = (View) findViewById(R.id._bg__android_large___4);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		_bg__group_13_ek1 = (View) findViewById(R.id._bg__group_13_ek1);
+		_2024 = (TextView) findViewById(R.id._2024);
+		_bg______icon__arrow_drop_down__ek1 = (View) findViewById(R.id._bg______icon__arrow_drop_down__ek1);
+		vector = (ImageView) findViewById(R.id.vector);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		_bg__group_34_ek1 = (View) findViewById(R.id._bg__group_34_ek1);
+		_bg__group_33_ek1 = (View) findViewById(R.id._bg__group_33_ek1);
+		_bg__group_32_ek1 = (View) findViewById(R.id._bg__group_32_ek1);
+		feb = (TextView) findViewById(R.id.feb);
+		_bg______icon__arrow_drop_down__ek3 = (View) findViewById(R.id._bg______icon__arrow_drop_down__ek3);
+		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+		vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
+		_bg_______icon__navigate_next__ek1 = (View) findViewById(R.id._bg_______icon__navigate_next__ek1);
+		vector_ek4 = (ImageView) findViewById(R.id.vector_ek4);
+		vector_ek5 = (ImageView) findViewById(R.id.vector_ek5);
+		gathering_usage_information_for_analysis_and_optimization_of_electric_vehicle_performance_ = (TextView) findViewById(R.id.gathering_usage_information_for_analysis_and_optimization_of_electric_vehicle_performance_);
+		_bg__group_20_ek1 = (View) findViewById(R.id._bg__group_20_ek1);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+		download_data = (TextView) findViewById(R.id.download_data);
+		_bg__group_22_ek1 = (View) findViewById(R.id._bg__group_22_ek1);
+		ola_s1 = (TextView) findViewById(R.id.ola_s1);
+		_bg______icon__arrow_drop_down__ek5 = (View) findViewById(R.id._bg______icon__arrow_drop_down__ek5);
+		vector_ek6 = (ImageView) findViewById(R.id.vector_ek6);
+		vector_ek7 = (ImageView) findViewById(R.id.vector_ek7);
+		recent_orders = (TextView) findViewById(R.id.recent_orders);
+		_bg__frame_5_ek1 = (View) findViewById(R.id._bg__frame_5_ek1);
+		_bg__group_45_ek1 = (View) findViewById(R.id._bg__group_45_ek1);
+		_bg__group_35_ek1 = (View) findViewById(R.id._bg__group_35_ek1);
+		rectangle_5_ek1 = (View) findViewById(R.id.rectangle_5_ek1);
+		order____87654321 = (TextView) findViewById(R.id.order____87654321);
+		february_10__2024___2_00_pm = (TextView) findViewById(R.id.february_10__2024___2_00_pm);
+		suji_ka_halwa_charging_station_iter_square__bhubneshwar__odisha = (TextView) findViewById(R.id.suji_ka_halwa_charging_station_iter_square__bhubneshwar__odisha);
+		_bg__group_44_ek1 = (View) findViewById(R.id._bg__group_44_ek1);
+		success = (TextView) findViewById(R.id.success);
+		rs_700 = (TextView) findViewById(R.id.rs_700);
+		_bg__group_46_ek1 = (View) findViewById(R.id._bg__group_46_ek1);
+		_bg__group_35_ek3 = (View) findViewById(R.id._bg__group_35_ek3);
+		rectangle_5_ek2 = (View) findViewById(R.id.rectangle_5_ek2);
+		order____87654321_ek1 = (TextView) findViewById(R.id.order____87654321_ek1);
+		february_10__2024___2_00_pm_ek1 = (TextView) findViewById(R.id.february_10__2024___2_00_pm_ek1);
+		suraj_prem_charging_station_jio_nagar__bhubneshwar__odisha = (TextView) findViewById(R.id.suraj_prem_charging_station_jio_nagar__bhubneshwar__odisha);
+		_bg__group_44_ek3 = (View) findViewById(R.id._bg__group_44_ek3);
+		success_ek1 = (TextView) findViewById(R.id.success_ek1);
+		rs_126 = (TextView) findViewById(R.id.rs_126);
+		_bg__group_40_ek1 = (View) findViewById(R.id._bg__group_40_ek1);
+		rectangle_5_ek3 = (View) findViewById(R.id.rectangle_5_ek3);
+		_bg__group_35_ek5 = (View) findViewById(R.id._bg__group_35_ek5);
+		order____12348765 = (TextView) findViewById(R.id.order____12348765);
+		february_18__2024___1_30_pm = (TextView) findViewById(R.id.february_18__2024___1_30_pm);
+		indhar_ka_ev_charging_station_saheed_nagar__bhubneshwar__odisha = (TextView) findViewById(R.id.indhar_ka_ev_charging_station_saheed_nagar__bhubneshwar__odisha);
+		_bg__group_36_ek1 = (View) findViewById(R.id._bg__group_36_ek1);
+		rs_537 = (TextView) findViewById(R.id.rs_537);
+		terminated = (TextView) findViewById(R.id.terminated);
+		_bg__group_43_ek1 = (View) findViewById(R.id._bg__group_43_ek1);
+		rectangle_5_ek4 = (View) findViewById(R.id.rectangle_5_ek4);
+		_bg__group_41_ek1 = (View) findViewById(R.id._bg__group_41_ek1);
+		order____12345678 = (TextView) findViewById(R.id.order____12345678);
+		february_21__2024___5_30_pm = (TextView) findViewById(R.id.february_21__2024___5_30_pm);
+		gajar_ka_halwa_charging_station_khao_gali__bhubneshwar__odisha = (TextView) findViewById(R.id.gajar_ka_halwa_charging_station_khao_gali__bhubneshwar__odisha);
+		_bg__group_42_ek1 = (View) findViewById(R.id._bg__group_42_ek1);
+		charging___ = (TextView) findViewById(R.id.charging___);
+		on_hold = (TextView) findViewById(R.id.on_hold);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
