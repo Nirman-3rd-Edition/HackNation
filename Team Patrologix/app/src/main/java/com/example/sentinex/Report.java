5ce888b5a2046c5ec8cfe06fa9ce83e919d899bf
rajee <kumarrajeevbehera2019@gmail.com>
2024-02-20 23:35:11 +0530

    Report Updated

--- Team Patrologix/app/src/main/java/com/example/sentinex/Report.java
+++ Team Patrologix/app/src/main/java/com/example/sentinex/Report.java
@@ -1,4 +1,35 @@
 package com.example.sentinex;
 
 public class Report {
+    private String description;
+    private String location;
+    private String time;
+    public Report(String description, String location, String time) {
+        this.description = description;
+        this.location = location;
+        this.time = time;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
