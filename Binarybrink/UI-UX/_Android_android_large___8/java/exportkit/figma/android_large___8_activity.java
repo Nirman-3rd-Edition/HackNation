8a8c61ca31b1210b5bcffe63b572c31d7b13eef6
Freak3123 <118867504+Freak3123@users.noreply.github.com>
2024-02-21 00:16:21 +0530

    Add files via upload

---
+++ Binarybrink/UI-UX/_Android_android_large___8/java/exportkit/figma/android_large___8_activity.java
@@ -0,0 +1,108 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_large___8
+	 *	@date 		Tuesday 20th of February 2024 05:05:16 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class android_large___8_activity extends Activity {
+
+	
+	private View _bg__android_large___8;
+	private View rectangle_1;
+	private View _bg_______icon__navigate_next__ek1;
+	private ImageView vector;
+	private ImageView vector_ek1;
+	private View _bg__group_61_ek1;
+	private TextView charging;
+	private TextView for_ola_s1;
+	private View _bg__group_19_ek1;
+	private View _bg__group_16_ek1;
+	private TextView _0_00;
+	private TextView tot__kwh;
+	private View _bg__group_17_ek1;
+	private TextView rs_0_00;
+	private TextView expend;
+	private View _bg__group_18_ek1;
+	private TextView _00_00_00;
+	private TextView duration;
+	private TextView recharging_electric_vehicle_for_extended_range_and_eco_friendly_transportation_;
+	private View _bg___group_20_ek1;
+	private View rectangle_5;
+	private TextView tap_to_stop;
+	private View _bg__group_59_ek1;
+	private View ellipse_3;
+	private View _bg__group_58_ek1;
+	private View ellipse_4;
+	private View _bg__property_1_default;
+	private View ellipse_5_ek4;
+	private TextView _75;
+	private TextView __;
+	private ImageView line_4;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_large___8);
+
+		
+		_bg__android_large___8 = (View) findViewById(R.id._bg__android_large___8);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		_bg_______icon__navigate_next__ek1 = (View) findViewById(R.id._bg_______icon__navigate_next__ek1);
+		vector = (ImageView) findViewById(R.id.vector);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		_bg__group_61_ek1 = (View) findViewById(R.id._bg__group_61_ek1);
+		charging = (TextView) findViewById(R.id.charging);
+		for_ola_s1 = (TextView) findViewById(R.id.for_ola_s1);
+		_bg__group_19_ek1 = (View) findViewById(R.id._bg__group_19_ek1);
+		_bg__group_16_ek1 = (View) findViewById(R.id._bg__group_16_ek1);
+		_0_00 = (TextView) findViewById(R.id._0_00);
+		tot__kwh = (TextView) findViewById(R.id.tot__kwh);
+		_bg__group_17_ek1 = (View) findViewById(R.id._bg__group_17_ek1);
+		rs_0_00 = (TextView) findViewById(R.id.rs_0_00);
+		expend = (TextView) findViewById(R.id.expend);
+		_bg__group_18_ek1 = (View) findViewById(R.id._bg__group_18_ek1);
+		_00_00_00 = (TextView) findViewById(R.id._00_00_00);
+		duration = (TextView) findViewById(R.id.duration);
+		recharging_electric_vehicle_for_extended_range_and_eco_friendly_transportation_ = (TextView) findViewById(R.id.recharging_electric_vehicle_for_extended_range_and_eco_friendly_transportation_);
+		_bg___group_20_ek1 = (View) findViewById(R.id._bg___group_20_ek1);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+		tap_to_stop = (TextView) findViewById(R.id.tap_to_stop);
+		_bg__group_59_ek1 = (View) findViewById(R.id._bg__group_59_ek1);
+		ellipse_3 = (View) findViewById(R.id.ellipse_3);
+		_bg__group_58_ek1 = (View) findViewById(R.id._bg__group_58_ek1);
+		ellipse_4 = (View) findViewById(R.id.ellipse_4);
+		_bg__property_1_default = (View) findViewById(R.id._bg__property_1_default);
+		ellipse_5_ek4 = (View) findViewById(R.id.ellipse_5_ek4);
+		_75 = (TextView) findViewById(R.id._75);
+		__ = (TextView) findViewById(R.id.__);
+		line_4 = (ImageView) findViewById(R.id.line_4);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
