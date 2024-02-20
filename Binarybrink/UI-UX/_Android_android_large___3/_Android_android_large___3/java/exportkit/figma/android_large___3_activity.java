a2a7c19e594a2dc0c734381bc5916bc044c2ed37
Freak3123 <118867504+Freak3123@users.noreply.github.com>
2024-02-20 22:40:11 +0530

    Add files via upload

---
+++ Binarybrink/UI-UX/_Android_android_large___3/_Android_android_large___3/java/exportkit/figma/android_large___3_activity.java
@@ -0,0 +1,176 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_large___3
+	 *	@date 		Tuesday 20th of February 2024 03:24:50 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class android_large___3_activity extends Activity {
+
+	
+	private View _bg__android_large___3;
+	private View rectangle_1;
+	private View _bg__group_13_ek1;
+	private TextView _2024;
+	private View _bg______icon__arrow_drop_down__ek1;
+	private ImageView vector;
+	private ImageView vector_ek1;
+	private View _bg__group_12_ek1;
+	private TextView february;
+	private View _bg______icon__arrow_drop_down__ek3;
+	private ImageView vector_ek2;
+	private ImageView vector_ek3;
+	private View _bg_______icon__navigate_next__ek1;
+	private ImageView vector_ek4;
+	private ImageView vector_ek5;
+	private TextView charging_stations_visited;
+	private TextView gathering_usage_information_for_analysis_and_optimization_of_electric_vehicle_performance_;
+	private View _bg__group_20_ek1;
+	private View rectangle_5;
+	private TextView download_data;
+	private View _bg__frame_4_ek1;
+	private View _bg__group_27_ek1;
+	private View _bg__group_26_ek1;
+	private TextView gajar_ka_halwa_charging_station;
+	private TextView february_21__2024___5_30_pm;
+	private View _bg__group_24_ek1;
+	private TextView khao_gali__bhubneshwar__odisha;
+	private ImageView vector_ek6;
+	private View _bg__group_25_ek1;
+	private View rectangle_4;
+	private View _bg__group_23_ek1;
+	private ImageView mask_group;
+	private View _bg__group_28_ek1;
+	private View _bg__group_26_ek3;
+	private TextView indhar_ka_ev_charging_station;
+	private TextView february_18__2024___1_30_pm;
+	private View _bg__group_24_ek3;
+	private TextView saheed_nagar__bhubneshwar__odisha;
+	private ImageView vector_ek7;
+	private View _bg__group_25_ek3;
+	private View rectangle_4_ek1;
+	private View _bg__group_23_ek3;
+	private ImageView mask_group_ek1;
+	private View _bg__group_29_ek1;
+	private View _bg__group_26_ek5;
+	private TextView suji_ka_halwa_charging_station;
+	private TextView february_10__2024___2_00_pm;
+	private View _bg__group_24_ek5;
+	private TextView iter_square__bhubneshwar__odisha;
+	private ImageView vector_ek8;
+	private View _bg__group_25_ek5;
+	private View rectangle_4_ek2;
+	private View _bg__group_23_ek5;
+	private ImageView mask_group_ek2;
+	private View _bg__group_30_ek1;
+	private View _bg__group_26_ek7;
+	private TextView suraj_prem_charging_station;
+	private TextView february_29__2024___7_00_pm;
+	private View _bg__group_24_ek7;
+	private TextView jio_nagar__bhubneshwar__odisha;
+	private ImageView vector_ek9;
+	private View _bg__group_25_ek7;
+	private View rectangle_4_ek3;
+	private View _bg__group_23_ek7;
+	private ImageView mask_group_ek3;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_large___3);
+
+		
+		_bg__android_large___3 = (View) findViewById(R.id._bg__android_large___3);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		_bg__group_13_ek1 = (View) findViewById(R.id._bg__group_13_ek1);
+		_2024 = (TextView) findViewById(R.id._2024);
+		_bg______icon__arrow_drop_down__ek1 = (View) findViewById(R.id._bg______icon__arrow_drop_down__ek1);
+		vector = (ImageView) findViewById(R.id.vector);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		_bg__group_12_ek1 = (View) findViewById(R.id._bg__group_12_ek1);
+		february = (TextView) findViewById(R.id.february);
+		_bg______icon__arrow_drop_down__ek3 = (View) findViewById(R.id._bg______icon__arrow_drop_down__ek3);
+		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+		vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
+		_bg_______icon__navigate_next__ek1 = (View) findViewById(R.id._bg_______icon__navigate_next__ek1);
+		vector_ek4 = (ImageView) findViewById(R.id.vector_ek4);
+		vector_ek5 = (ImageView) findViewById(R.id.vector_ek5);
+		charging_stations_visited = (TextView) findViewById(R.id.charging_stations_visited);
+		gathering_usage_information_for_analysis_and_optimization_of_electric_vehicle_performance_ = (TextView) findViewById(R.id.gathering_usage_information_for_analysis_and_optimization_of_electric_vehicle_performance_);
+		_bg__group_20_ek1 = (View) findViewById(R.id._bg__group_20_ek1);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+		download_data = (TextView) findViewById(R.id.download_data);
+		_bg__frame_4_ek1 = (View) findViewById(R.id._bg__frame_4_ek1);
+		_bg__group_27_ek1 = (View) findViewById(R.id._bg__group_27_ek1);
+		_bg__group_26_ek1 = (View) findViewById(R.id._bg__group_26_ek1);
+		gajar_ka_halwa_charging_station = (TextView) findViewById(R.id.gajar_ka_halwa_charging_station);
+		february_21__2024___5_30_pm = (TextView) findViewById(R.id.february_21__2024___5_30_pm);
+		_bg__group_24_ek1 = (View) findViewById(R.id._bg__group_24_ek1);
+		khao_gali__bhubneshwar__odisha = (TextView) findViewById(R.id.khao_gali__bhubneshwar__odisha);
+		vector_ek6 = (ImageView) findViewById(R.id.vector_ek6);
+		_bg__group_25_ek1 = (View) findViewById(R.id._bg__group_25_ek1);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		_bg__group_23_ek1 = (View) findViewById(R.id._bg__group_23_ek1);
+		mask_group = (ImageView) findViewById(R.id.mask_group);
+		_bg__group_28_ek1 = (View) findViewById(R.id._bg__group_28_ek1);
+		_bg__group_26_ek3 = (View) findViewById(R.id._bg__group_26_ek3);
+		indhar_ka_ev_charging_station = (TextView) findViewById(R.id.indhar_ka_ev_charging_station);
+		february_18__2024___1_30_pm = (TextView) findViewById(R.id.february_18__2024___1_30_pm);
+		_bg__group_24_ek3 = (View) findViewById(R.id._bg__group_24_ek3);
+		saheed_nagar__bhubneshwar__odisha = (TextView) findViewById(R.id.saheed_nagar__bhubneshwar__odisha);
+		vector_ek7 = (ImageView) findViewById(R.id.vector_ek7);
+		_bg__group_25_ek3 = (View) findViewById(R.id._bg__group_25_ek3);
+		rectangle_4_ek1 = (View) findViewById(R.id.rectangle_4_ek1);
+		_bg__group_23_ek3 = (View) findViewById(R.id._bg__group_23_ek3);
+		mask_group_ek1 = (ImageView) findViewById(R.id.mask_group_ek1);
+		_bg__group_29_ek1 = (View) findViewById(R.id._bg__group_29_ek1);
+		_bg__group_26_ek5 = (View) findViewById(R.id._bg__group_26_ek5);
+		suji_ka_halwa_charging_station = (TextView) findViewById(R.id.suji_ka_halwa_charging_station);
+		february_10__2024___2_00_pm = (TextView) findViewById(R.id.february_10__2024___2_00_pm);
+		_bg__group_24_ek5 = (View) findViewById(R.id._bg__group_24_ek5);
+		iter_square__bhubneshwar__odisha = (TextView) findViewById(R.id.iter_square__bhubneshwar__odisha);
+		vector_ek8 = (ImageView) findViewById(R.id.vector_ek8);
+		_bg__group_25_ek5 = (View) findViewById(R.id._bg__group_25_ek5);
+		rectangle_4_ek2 = (View) findViewById(R.id.rectangle_4_ek2);
+		_bg__group_23_ek5 = (View) findViewById(R.id._bg__group_23_ek5);
+		mask_group_ek2 = (ImageView) findViewById(R.id.mask_group_ek2);
+		_bg__group_30_ek1 = (View) findViewById(R.id._bg__group_30_ek1);
+		_bg__group_26_ek7 = (View) findViewById(R.id._bg__group_26_ek7);
+		suraj_prem_charging_station = (TextView) findViewById(R.id.suraj_prem_charging_station);
+		february_29__2024___7_00_pm = (TextView) findViewById(R.id.february_29__2024___7_00_pm);
+		_bg__group_24_ek7 = (View) findViewById(R.id._bg__group_24_ek7);
+		jio_nagar__bhubneshwar__odisha = (TextView) findViewById(R.id.jio_nagar__bhubneshwar__odisha);
+		vector_ek9 = (ImageView) findViewById(R.id.vector_ek9);
+		_bg__group_25_ek7 = (View) findViewById(R.id._bg__group_25_ek7);
+		rectangle_4_ek3 = (View) findViewById(R.id.rectangle_4_ek3);
+		_bg__group_23_ek7 = (View) findViewById(R.id._bg__group_23_ek7);
+		mask_group_ek3 = (ImageView) findViewById(R.id.mask_group_ek3);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
