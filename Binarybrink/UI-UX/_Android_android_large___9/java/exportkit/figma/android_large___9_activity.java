56fcce3470cc493bd20094d50a6a1af6036cac14
Freak3123 <118867504+Freak3123@users.noreply.github.com>
2024-02-21 00:44:17 +0530

    Add files via upload

---
+++ Binarybrink/UI-UX/_Android_android_large___9/java/exportkit/figma/android_large___9_activity.java
@@ -0,0 +1,116 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_large___9
+	 *	@date 		Tuesday 20th of February 2024 07:09:43 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class android_large___9_activity extends Activity {
+
+	
+	private View _bg__android_large___9;
+	private View rectangle_1;
+	private View _bg_______icon__navigate_next__ek1;
+	private ImageView vector;
+	private ImageView vector_ek1;
+	private View _bg__group_65_ek1;
+	private TextView charge_done;
+	private TextView for_ola_s1;
+	private View _bg__group_19_ek1;
+	private View _bg__group_16_ek1;
+	private TextView _23_01;
+	private TextView tot__kwh;
+	private View _bg__group_17_ek1;
+	private TextView rs_156_00;
+	private TextView expend;
+	private View _bg__group_18_ek1;
+	private TextView _00_35_40;
+	private TextView duration;
+	private TextView sustainable_recharge_for_extended_vehicle_driving_range_;
+	private View _bg___group_20_ek1;
+	private View _bg__group_66_ek1;
+	private View rectangle_5;
+	private TextView pay;
+	private View _bg__group_64_ek1;
+	private View rectangle_6;
+	private View _bg__group_ek1;
+	private ImageView vector_ek2;
+	private View _bg__group_63_ek1;
+	private ImageView vector_ek3;
+	private ImageView vector_ek4;
+	private ImageView vector_ek5;
+	private View _bg__group_59_ek1;
+	private View _bg__group_58_ek1;
+	private View ellipse_3;
+	private TextView __184_08;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_large___9);
+
+		
+		_bg__android_large___9 = (View) findViewById(R.id._bg__android_large___9);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		_bg_______icon__navigate_next__ek1 = (View) findViewById(R.id._bg_______icon__navigate_next__ek1);
+		vector = (ImageView) findViewById(R.id.vector);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		_bg__group_65_ek1 = (View) findViewById(R.id._bg__group_65_ek1);
+		charge_done = (TextView) findViewById(R.id.charge_done);
+		for_ola_s1 = (TextView) findViewById(R.id.for_ola_s1);
+		_bg__group_19_ek1 = (View) findViewById(R.id._bg__group_19_ek1);
+		_bg__group_16_ek1 = (View) findViewById(R.id._bg__group_16_ek1);
+		_23_01 = (TextView) findViewById(R.id._23_01);
+		tot__kwh = (TextView) findViewById(R.id.tot__kwh);
+		_bg__group_17_ek1 = (View) findViewById(R.id._bg__group_17_ek1);
+		rs_156_00 = (TextView) findViewById(R.id.rs_156_00);
+		expend = (TextView) findViewById(R.id.expend);
+		_bg__group_18_ek1 = (View) findViewById(R.id._bg__group_18_ek1);
+		_00_35_40 = (TextView) findViewById(R.id._00_35_40);
+		duration = (TextView) findViewById(R.id.duration);
+		sustainable_recharge_for_extended_vehicle_driving_range_ = (TextView) findViewById(R.id.sustainable_recharge_for_extended_vehicle_driving_range_);
+		_bg___group_20_ek1 = (View) findViewById(R.id._bg___group_20_ek1);
+		_bg__group_66_ek1 = (View) findViewById(R.id._bg__group_66_ek1);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+		pay = (TextView) findViewById(R.id.pay);
+		_bg__group_64_ek1 = (View) findViewById(R.id._bg__group_64_ek1);
+		rectangle_6 = (View) findViewById(R.id.rectangle_6);
+		_bg__group_ek1 = (View) findViewById(R.id._bg__group_ek1);
+		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+		_bg__group_63_ek1 = (View) findViewById(R.id._bg__group_63_ek1);
+		vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
+		vector_ek4 = (ImageView) findViewById(R.id.vector_ek4);
+		vector_ek5 = (ImageView) findViewById(R.id.vector_ek5);
+		_bg__group_59_ek1 = (View) findViewById(R.id._bg__group_59_ek1);
+		_bg__group_58_ek1 = (View) findViewById(R.id._bg__group_58_ek1);
+		ellipse_3 = (View) findViewById(R.id.ellipse_3);
+		__184_08 = (TextView) findViewById(R.id.__184_08);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
