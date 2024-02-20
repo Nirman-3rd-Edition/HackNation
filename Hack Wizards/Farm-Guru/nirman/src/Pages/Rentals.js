1899bbf166df3209e6ffcd5679e124a27fdf330a
Aariyan-Kumar <cst.21bctc85@silicon.ac.in>
2024-02-20 19:27:31 +0530

    new files added
    2nd commit

---
+++ Hack Wizards/Farm-Guru/nirman/src/Pages/Rentals.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+
+export default function Rentals() {
+    return (
+        <>
+            <Header />
+            <h1>Rentals Page</h1>
+            <Footer />
+        </>
+    )
+}
