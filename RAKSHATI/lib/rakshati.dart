670522a7007e6c95dc862f01511dde5606811e0f
Raj Sahasransu Biswal <guruprasad4646@gmail.com>
2024-02-21 00:03:29 +0530

    update

--- RAKSHATI/lib/rakshati.dart
+++ RAKSHATI/lib/rakshati.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:location/location.dart';
+import 'package:sms/sms.dart';
 
 void main() {
   runApp(SOSApp());
@@ -12,8 +15,6 @@ class SOSApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('SOS App'),
         ),
-      )
-    )
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -23,6 +24,73 @@ class SOSApp extends StatelessWidget {
                 onPressed: () => _callNumber('911'), // Police
                 child: Text('Call Police'),
               ),
-          )
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _callNumber('911'), // Emergency Home
+                child: Text('Call Emergency Home'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _callNumber('911'), // Ambulance
+                child: Text('Call Ambulance'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _sendSmsAlert,
+                child: Text('Send SMS Alert'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _getCurrentLocation,
+                child: Text('Get Current Location'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _openMap(0, 0), // Open Map
+                child: Text('Open Map'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
+  void _callNumber(String number) async {
+    String url = 'tel:$number';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  void _sendSmsAlert() {
+    String message = 'I need help! Please contact me ASAP.';
+    List<String> recipients = ['1234567890', '0987654321']; // Add your emergency contacts here
+    recipients.forEach((recipient) {
+      SmsSender sender = new SmsSender();
+      sender.sendSms(new SmsMessage(recipient, message));
+    });
+  }
+
+  void _getCurrentLocation() async {
+    var location = Location();
+    try {
+      var userLocation = await location.getLocation();
+      print('Latitude: ${userLocation.latitude}, Longitude: ${userLocation.longitude}');
+      // You can display the location on the UI or use it for further processing
+    } catch (e) {
+      print('Could not get the location: $e');
+    }
+  }
+
+  void _openMap(double lat, double lng) async {
+    String url = 'https://www.google.com/maps/search/?api=1&query=$lat,$lng';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not open map';
+    }
+  }
 }
--- RAKSHATI/macos/Flutter/GeneratedPluginRegistrant.swift
+++ RAKSHATI/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import location
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
