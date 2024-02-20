eee8661a7c4bd81ffdb4bf04a0a9b52694b213bd
Suveshh <125795082+Suveshh@users.noreply.github.com>
2024-02-21 02:54:10 +0530

    Add files via upload

---
+++ TeleTubbies/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import sqlite3
+from datetime import datetime
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', selected_date='', no_data=False)
+
+@app.route('/attendance', methods=['POST'])
+def attendance():
+    selected_date = request.form.get('selected_date')
+    selected_date_obj = datetime.strptime(selected_date, '%Y-%m-%d')
+    formatted_date = selected_date_obj.strftime('%Y-%m-%d')
+
+    conn = sqlite3.connect('attendance.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT name, time FROM attendance WHERE date = ?", (formatted_date,))
+    attendance_data = cursor.fetchall()
+
+    conn.close()
+
+    if not attendance_data:
+        return render_template('index.html', selected_date=selected_date, no_data=True)
+    
+    return render_template('index.html', selected_date=selected_date, attendance_data=attendance_data)
+
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=8080)
