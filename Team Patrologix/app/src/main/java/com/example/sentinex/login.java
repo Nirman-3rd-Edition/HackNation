0503062c539f6b365328054a53fd7d741ee328a4
Yajy <goswamiyajy@gmail.com>
2024-02-21 09:15:49 +0530

    login try part2

--- Team Patrologix/app/src/main/java/com/example/sentinex/login.java
+++ Team Patrologix/app/src/main/java/com/example/sentinex/login.java
@@ -1,5 +1,8 @@
 package com.example.sentinex;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,19 +13,90 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
 
+    TextView logsignup;
+    Button button;
+    EditText email, password;
+    FirebaseAuth auth;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    android.app.ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please Wait...");
+        progressDialog.setCancelable(false);
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logbutton);
+        email = findViewById(R.id.editTexLogEmail);
+        password = findViewById(R.id.editTextLogPassword);
+        logsignup = findViewById(R.id.logsignup);
+
+        logsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this,registration.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Email = email.getText().toString();
+                String pass = password.getText().toString();
+
+                if ((TextUtils.isEmpty(Email))){
+                    progressDialog.dismiss();
+                    Toast.makeText(login.this, "Enter The Email", Toast.LENGTH_SHORT).show();
+                }else if (TextUtils.isEmpty(pass)){
+                    progressDialog.dismiss();
+                    Toast.makeText(login.this, "Enter The Password", Toast.LENGTH_SHORT).show();
+                }else if (!Email.matches(emailPattern)){
+                    progressDialog.dismiss();
+                    email.setError("Give Proper Email Address");
+                }else if (password.length()<6){
+                    progressDialog.dismiss();
+                    password.setError("More Then Six Characters");
+                    Toast.makeText(login.this, "Password Needs To Be Longer Then Six Characters", Toast.LENGTH_SHORT).show();
+                }else {
+                    auth.signInWithEmailAndPassword(Email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                progressDialog.show();
+                                try {
+                                    Intent intent = new Intent(login.this , MainActivity.class);
+                                    intent.putExtra("key",Email);//rajeev yeh use karenge as key (email)...yeh pass ho raha hai next activity mei
+                                    startActivity(intent);
+                                    finish();
+                                }catch (Exception e){
+                                    Toast.makeText(login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }else {
+                                Toast.makeText(login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
 
 
+            }
+        });
+    }
 }
 
 //
-//package com.example.sentenix;
+//package com.example.sentenix_prototype2;
 //
 //public class Report {
 //    private String description;
@@ -45,4 +119,3 @@ public class login extends AppCompatActivity {
 //        return location;
 //    }
 //}
-
