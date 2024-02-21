7ca7b2f4932a996806e987d573e31a34c91076bb
Dharmesh Raj <rajdharmesh666@gmail.com>
2024-02-21 07:55:59 +0530

    5th commit Dharmesh created fragment

---
+++ Team Patrologix/app/src/main/java/com/example/sentinex/Fragments/SearchFragment.java
@@ -0,0 +1,29 @@
+package com.example.sentinex.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.sentenix_prototype2.R;
+
+
+
+public class SearchFragment extends Fragment {
+
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
